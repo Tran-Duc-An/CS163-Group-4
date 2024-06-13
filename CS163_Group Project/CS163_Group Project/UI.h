a85b60d9669f38f89cb91ce877891455365bc3e5
Tran-Duc-An <161696114+Tran-Duc-An@users.noreply.github.com>
2024-06-13 10:16:32 +0700

    create dataset

--- CS163_Group Project/CS163_Group Project/UI.h
+++ CS163_Group Project/CS163_Group Project/UI.h
@@ -1,34 +1,34 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-struct Button {
-	sf::Texture texture;
-	sf::Sprite sprite;
-	Button(int x, int y, std::string imagePath);
-	bool isClicked(sf::RenderWindow& window, sf::Event event);
-	void draw(sf::RenderWindow& window);
-};
-
-
-
-struct InputBox : Button {
-	sf::Text text;
-	sf::Font font;
-	sf::Text textDisplay;
-	sf::Text nameHolder;
-	std::string name;
-	bool active = 0;
-	InputBox(int x, int y, std::string imagePath, std::string name);
-	void isClicked(sf::RenderWindow& window, sf::Event& event);
-	void draw(sf::RenderWindow& window);
-};
-
-struct SubmitButton : Button {
-	bool isClicked(sf::RenderWindow& window, sf::Event& event, std::string& word, sf::Text& text);
-	SubmitButton(int x, int y, std::string imagePath);
-};
-
-struct InputDef : InputBox {
-	InputDef(int x, int y, std::string imagePath, std::string name);
-	void draw(sf::RenderWindow& window);
-	void isClicked(sf::RenderWindow& window, sf::Event& event);
-};
+//#pragma once
+//#include <SFML/Graphics.hpp>
+//struct Button {
+//	sf::Texture texture;
+//	sf::Sprite sprite;
+//	Button(int x, int y, std::string imagePath);
+//	bool isClicked(sf::RenderWindow& window, sf::Event event);
+//	void draw(sf::RenderWindow& window);
+//};
+//
+//
+//
+//struct InputBox : Button {
+//	sf::Text text;
+//	sf::Font font;
+//	sf::Text textDisplay;
+//	sf::Text nameHolder;
+//	std::string name;
+//	bool active = 0;
+//	InputBox(int x, int y, std::string imagePath, std::string name);
+//	void isClicked(sf::RenderWindow& window, sf::Event& event);
+//	void draw(sf::RenderWindow& window);
+//};
+//
+//struct SubmitButton : Button {
+//	bool isClicked(sf::RenderWindow& window, sf::Event& event, std::string& word, sf::Text& text);
+//	SubmitButton(int x, int y, std::string imagePath);
+//};
+//
+//struct InputDef : InputBox {
+//	InputDef(int x, int y, std::string imagePath, std::string name);
+//	void draw(sf::RenderWindow& window);
+//	void isClicked(sf::RenderWindow& window, sf::Event& event);
+//};

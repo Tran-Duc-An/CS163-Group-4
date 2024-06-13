aec5d2f15f3efbdecde25bd6cb80019d070f7759
Thinh313 <144980730+Thinh313@users.noreply.github.com>
2024-06-13 11:59:11 +0700

    Update functions to insert and seacrch a keyword in the trie

--- CS163_Group Project/CS163_Group Project/main.cpp
+++ CS163_Group Project/CS163_Group Project/main.cpp
@@ -1,81 +1,64 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <fstream>
 using namespace std;
 
-struct trie {
-	vector<char> letters;
+struct trie{
+
 	vector<trie*>  children;
-	bool isend = 0;
-	string definition;
+	bool isend = false;
+	vector<string> definition;
+	trie() : children(26, nullptr) {}
 };
 
-void insert(trie*& root, string s, int k, int pos, string definition) {
-	if (k == pos) {
-		if (root == nullptr) {
-			root = new trie;
-			root->letters.push_back(s[pos]);
-			root->children.push_back(nullptr);
-		}
-		else {
-			bool flag = 0;
-			for (int i = 0; i < root->letters.size(); i++) {
-				if (s[pos] == root->letters[i]) flag = 1;
-			}
-			if (flag == 0) {
-				root->letters.push_back(s[pos]);
-				root->children.push_back(nullptr);
-			}
-		}
-		if (pos == s.length() - 1) {
-			root->isend = 1;
-			root->definition = definition;
-		}
-	}
-	else {
-		for (int i = 0; i < root->letters.size(); i++) {
-			if (s[k] == root->letters[i]) insert(root->children[i], s, k + 1, pos,definition);
-		}
-	}
-}
 
-void createtrie(trie*& root, vector<string> s, vector<string> def) {
-	for (int i = 0; i < s.size(); i++) {
-		for (int j = 0; j < s[i].length(); j++) insert(root, s[i], 0, j, def[i]);
+string toLowerCase(string& str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] >= 'A' && str[i] <= 'Z') str[i] += 32;
 	}
+	return str;
 }
-
-bool find(trie* root, string s, int k) {
-	if (root == nullptr) return 0;
-	if (k == s.length() - 1 && root->isend == 1) {
-		return 1;
-	}
-	else if (k == s.length() - 1 && root->isend == 0) return 0;
-	for (int i = 0; i < root->letters.size(); i++) {
-		if (s[k] == root->letters[i]) return find(root->children[i], s, k + 1);
-	}
-	return 0;
-}
-
-void deletetrie(trie*& root) {
-	if (root == nullptr) return;
-	for (int i = 0; i < root->children.size(); i++)
+void insertWord(trie*& root, string&word, string&definition)
+{
+	word = toLowerCase(word);
+	trie* current = root;
+	for (char c : word)
 	{
-		deletetrie(root->children[i]);
-		delete root;
+		if (current->children[c - 'a'] == nullptr)
+		{
+			current->children[c - 'a'] = new trie;
+		}
+		current = current->children[c - 'a'];
 	}
-	root = nullptr;
+	current->isend = true;
+	current->definition.push_back(definition);
 }
-
-int main() {
-	vector<string> s = { "apple", "an", "atomic" };
-	vector<string> def = { "a round fruit with shiny red or green skin that is fairly hard and white inside",
-		"used instead of a when the following word begins with a vowel sound",
-		"using the energy that is created when an atom is divided" };
-	trie* root = nullptr;
-	createtrie(root, s,def);
-	string ss = "apple";
-	cout << find(root, ss, 0);
-	deletetrie(root);
-	return 0;
+bool findWordMeaning(trie* root, string& word, vector<string>& meaning)
+{
+	word = toLowerCase(word);
+	trie* current = root;
+	for (char c : word)
+	{
+		if (current->children[c - 'a'] == nullptr)
+		{
+			return false;
+		}
+		current = current->children[c - 'a'];
+	}
+	if (current->isend)
+	{
+		meaning = current->definition;
+		return true;
+	}
+	return false;
 }
-
+void deleteTrie(trie* root)
+{
+	if (root == nullptr) return;
+	for (trie* child : root->children)
+	{
+		deleteTrie(child);
+	}
+	delete root;
+}

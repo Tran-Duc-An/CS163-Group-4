c0f373c0029bd8fbca7aeb2028cc0ab6c3cbfaeb
Dương Đình Trí Quang <161907590+ofdquantum@users.noreply.github.com>
2024-06-28 19:50:51 +0700

    Adding Vtrie

--- CS163_Group Project/CS163_Group Project/Trie.h
+++ CS163_Group Project/CS163_Group Project/Trie.h
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <cstring>
 #include <chrono>
+#include <io.h>
+#include<fcntl.h>
 
 using namespace std;
 
@@ -17,6 +19,15 @@ struct trie
 	int numChildren = 0;
 	trie() : children(38, nullptr) {}
 };
+
+struct Vtrie
+{
+	vector<Vtrie*> children;
+	vector<wstring> definition;
+	int numChildren = 0;
+	// 89 vietnamese characters and 2 for hyphen and space
+	Vtrie() : children(91, nullptr) {}
+};
 string toLowerCase(string& str);
 string removeSpecialCharacters(string& str);
 void insertWord(trie*& root, string& word, string& definition);
@@ -25,3 +36,8 @@ bool loadRawData(trie*& root);
 void loadTrie(trie*& root, ifstream& fin);
 void saveTrie(trie* root, ofstream& fout);
 void deleteTrie(trie*& root);
+wstring VToLower(wstring& str);
+void fillMap();
+void VInsertWord(Vtrie*& root, wstring& word, wstring& definition);
+bool VFindWordMeaning(Vtrie* root, wstring& word, vector<wstring>& meaning);
+void VDeleteTrie(Vtrie*& root);

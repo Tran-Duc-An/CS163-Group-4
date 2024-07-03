1f3ae9d25d6a822ca6d55163480d9992d0c2a4d6
Thinh313 <144980730+Thinh313@users.noreply.github.com>
2024-07-03 21:34:56 +0700

    Update functions.h

--- CS163_Group Project/CS163_Group Project/functions.h
+++ CS163_Group Project/CS163_Group Project/functions.h
@@ -32,19 +32,29 @@ struct Vtrie
 	// 89 vietnamese characters and 2 for hyphen and space
 	Vtrie() : children(91, nullptr) {}
 };
-
+// ENG-ENG
 string toLowerCase(string&str);
 string removeSpecialCharacters(string&str);
 void insertWord(trie*& root, string& word, string& definition);
+trie* findWord(trie* root, string& word);
 bool findWordMeaning(trie* root, string& word, vector<string>& meaning);
+bool changeWordDefinition(trie* root, string& word, string& newDefinition, int indexOfOldDefinitionToBeReplaced);
+void helperDeleteAWord(trie* root, string& word);
+bool deleteAWord(trie* root, string& word);
 bool loadRawData(trie*& root);
 void loadTrie(trie*& root, ifstream& fin);
 void saveTrie(trie* root, ofstream& fout);
 void deleteTrie(trie*& root);
 wstring VToLower(wstring&str);
+
+// VIET-ENG
 void fillMap();
 void VInsertWord(Vtrie*& root, wstring& word,wstring& definition);
+Vtrie* VFindWord(Vtrie* root, wstring& word);
 bool VFindWordMeaning(Vtrie* root, wstring& word, vector<wstring>& meaning);
+bool VChangeWordDefinition(Vtrie* root, wstring& word, wstring& newDefinition, int indexOfOldDefinitionToBeReplaced);
+void helperDeleteAWord(Vtrie* root, wstring& word);
+bool VDeleteAWord(Vtrie* root, wstring& word);
 void VDeleteTrie(Vtrie*& root);
 bool VloadRawData(Vtrie*& root);
 void saveVtrie(Vtrie* root, wofstream& fout);

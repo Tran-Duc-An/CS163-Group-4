ec09344c424c7a1dec8aa4da2a100dbfb5ded753
Thinh313 <144980730+Thinh313@users.noreply.github.com>
2024-07-26 21:53:41 +0700

    sync with an

--- CS163_Group Project/CS163_Group Project/main.cpp
+++ CS163_Group Project/CS163_Group Project/main.cpp
@@ -1,4 +1,16 @@
-#include "functions.h"
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <io.h>
+#include <fcntl.h>
+#include <locale>
+#include <codecvt>
+#include "Function.h"
+#include "UI.h";
+using namespace std;
 
-
-
+int main() {
+	run();
+	return 0;
+}

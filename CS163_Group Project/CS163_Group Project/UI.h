b678ba8b24a9e61852d2275d56f9fef8d4398c0f
Thinh313 <144980730+Thinh313@users.noreply.github.com>
2024-07-26 21:51:27 +0700

    Update UI.h

--- CS163_Group Project/CS163_Group Project/UI.h
+++ CS163_Group Project/CS163_Group Project/UI.h
@@ -1,34 +1,67 @@
-//#pragma once
-//#include <SFML/Graphics.hpp>
-//struct Button {
-//	sf::Texture texture;
-//	sf::Sprite sprite;
-//	Button(int x, int y, std::string imagePath);
-//	bool isClicked(sf::RenderWindow& window, sf::Event event);
-//	void draw(sf::RenderWindow& window);
-//};
-//
-//
-//
-//struct InputBox : Button {
-//	sf::Text text;
-//	sf::Font font;
-//	sf::Text textDisplay;
-//	sf::Text nameHolder;
-//	std::string name;
-//	bool active = 0;
-//	InputBox(int x, int y, std::string imagePath, std::string name);
-//	void isClicked(sf::RenderWindow& window, sf::Event& event);
-//	void draw(sf::RenderWindow& window);
-//};
-//
-//struct SubmitButton : Button {
-//	bool isClicked(sf::RenderWindow& window, sf::Event& event, std::string& word, sf::Text& text);
-//	SubmitButton(int x, int y, std::string imagePath);
-//};
-//
-//struct InputDef : InputBox {
-//	InputDef(int x, int y, std::string imagePath, std::string name);
-//	void draw(sf::RenderWindow& window);
-//	void isClicked(sf::RenderWindow& window, sf::Event& event);
-//};
+#pragma once
+#include <SFML/Graphics.hpp>
+
+struct Button {
+	sf::Texture texture;
+	sf::Sprite sprite;
+	std::string orgImage;
+	Button(int x, int y, std::string imagePath);
+	bool isClicked(sf::RenderWindow& window, sf::Event event);
+	void draw(sf::RenderWindow& window);
+	void isHover(sf::RenderWindow& window,std::string path);
+};
+
+
+
+struct InputBox : Button {
+	sf::Text text;
+	sf::Font font;
+	sf::Text textDisplay;
+	sf::Text nameHolder;
+	std::wstring name;
+	bool active = 0;
+	InputBox(int x, int y, std::string imagePath, std::wstring name);
+	void isClicked(sf::RenderWindow& window, sf::Event& event);
+	void draw(sf::RenderWindow& window);
+	void pasteFromClipboard();
+};
+
+struct SubmitVNButton : Button {
+	bool isClicked(sf::RenderWindow& window, sf::Event& event, std::wstring& word, sf::Text& text);
+	SubmitVNButton(int x, int y, std::string imagePath);
+};
+
+struct SubmitENButton:Button {
+	bool isClicked(sf::RenderWindow& window, sf::Event& event, std::string& word, sf::Text& text);
+	SubmitENButton(int x, int y, std::string imagePath);
+
+};
+
+
+struct InputDef : InputBox {
+	int numRow = 1;
+	int numChar = 0;
+	InputDef(int x, int y, std::string imagePath, std::wstring name,int numRows,int numChars);
+	void draw(sf::RenderWindow& window);
+	void isClicked(sf::RenderWindow& window, sf::Event& event);
+};
+
+struct ChoiceButton :Button {
+	std::wstring content;
+	int xx = 0;
+	int yy = 0;
+	ChoiceButton(int x, int y, std::string imagePath);
+	void draw(sf::RenderWindow& window);
+}; 
+struct AnswerButton :Button {
+	bool isRightAnswer = 0;
+	std::wstring content;
+	int xx = 0;
+	int yy = 0;
+
+	AnswerButton(int x, int y, std::string imagePath);
+	void draw(sf::RenderWindow& window);
+	int isClicked(sf::RenderWindow& window, sf::Event event);
+};
+
+int run();
